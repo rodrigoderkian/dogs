f22b793953f943e180e7111e4b440337e31d0ade
rodrigoderkian <derkiandev@gmail.com>
2024-02-07 15:21:37 -0300

    feat: useForm

---
+++ src/Hooks/useForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    message: 'Preencha um email válido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  const validate = (value) => {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+    } else {
+      setError('');
+      return true;
+    }
+  };
+
+  const onChange = ({ target }) => {
+    if (!!error) validate(target.value);
+    setValue(target.value);
+  };
+
+  return {
+    value,
+    setValue,
+    onChange,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+    error,
+  };
+};
+
+export default useForm;
